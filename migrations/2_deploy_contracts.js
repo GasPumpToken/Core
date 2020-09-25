aa99cafc4984a695c9bdab9b2162e763e6227510
DeFiDev <39846094+defideveloper@users.noreply.github.com>
2020-09-25 19:31:33 +0200

    Uniswap Fork core

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,14 +1,14 @@
 const Factory = artifacts.require('UniswapV2Factory.sol');
-const PumpToken = artifacts.require('PumpToken.sol');
-const DumpToken = artifacts.require('DumpToken.sol');
+const Token1 = artifacts.require('Token1.sol');
+const Token2 = artifacts.require('Token2.sol');
 
 module.exports = async function (deployer, _network, addresses) {
   await deployer.deploy(Factory, addresses[0]);
   const factory = await Factory.deployed();
 
-  await deployer.deploy(PumpToken);
-  await deployer.deploy(DumpToken);
-  const PumpToken = await PumpToken.deployed();
-  const DumpToken = await DumpToken.deployed();
-  await factory.createPair(PumpToken.address, DumpToken.address);
+  await deployer.deploy(Token1);
+  await deployer.deploy(Token2);
+  const token1 = await Token1.deployed();
+  const token2 = await Token2.deployed();
+  await factory.createPair(token1.address, token2.address);
 };
